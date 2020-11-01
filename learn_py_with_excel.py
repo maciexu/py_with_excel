ea402bbcc8c8449d74326f0408cdeab59c86ddd2
maciexu <43970987+maciexu@users.noreply.github.com>
2020-11-01 00:41:21 -0400

    Update learn_py_with_excel.py

--- learn_py_with_excel.py
+++ learn_py_with_excel.py
@@ -7,5 +7,6 @@ S1 = pd.Series(['a', 'b', 'c', 'd', 'e'])
 print(S1)
 
 df = pd.read_excel(r'C:\Users\mengx\Downloads\Data_Extract_From_Education_Statistics_-_All_Indicators.xlsx')
+df = pd.read_excel(r'C:\Users\mengx\Downloads\Data_Extract_From_Education_Statistics_-_All_Indicators.xlsx', sheetname = 'Data')
 print(df)
 
