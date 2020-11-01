77ecdaeffe48cbc08561c3b159e5dc0f8cce6af3
maciexu <43970987+maciexu@users.noreply.github.com>
2020-11-01 00:29:49 -0400

    Add files via upload
    read

---
+++ learn_py_with_excel.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Series
+print("-"*30 + "Section 1 Series" + "-"*30)
+
+S1 = pd.Series(['a', 'b', 'c', 'd', 'e'])
+print(S1)
+
+df = pd.read_excel(r'C:\Users\mengx\Downloads\Data_Extract_From_Education_Statistics_-_All_Indicators.xlsx')
+print(df)
+
